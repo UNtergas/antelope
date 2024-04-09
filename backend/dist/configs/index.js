c2c3bb86e6de1f0a7912688c2476fde65782fee8
UNtergas <mycomputerer135@gmail.com>
2024-04-09 21:57:19 +0200

    fix build->serve

--- backend/dist/configs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-const cors_config_1 = __importDefault(require("./cors.config"));
-exports.config = { corsOptions: cors_config_1.default };
-//# sourceMappingURL=index.js.map
--- backend/dist/service/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const antelope_service_1 = require("./antelope.service");
-const Service = {
-    AntelopeService: antelope_service_1.AntelopeService
-};
-exports.default = Service;
-//# sourceMappingURL=index.js.map
